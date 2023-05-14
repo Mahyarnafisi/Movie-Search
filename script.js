d752a4d3f824cdc2620a2d1dd497427a945feca5
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-05-14 12:21:08 +0200

    almost finish fetching from API and add not-found section with refresh button in it

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
 "use strict";
 // --------------------------------------//
+let movieNameRef = document.getElementById("movie-name");
+let btnSearch = document.getElementById("btn-search");
+const btnRefresh = document.querySelector(".btn-refresh");
+let containerResult = document.querySelector(".result");
+const containerNotFound = document.querySelector(".not-found");
+
+const apiKey = "4fccaa42";
+
+// function for fetching from API---------
+
+btnRefresh.addEventListener("click", () => {
+  window.location.reload();
+});
+
+const getMovie = () => {
+  const movieName = movieNameRef.value;
+  const apiUrl = `https://www.omdbapi.com/?i=${movieName}&apikey=${apiKey}`;
+  console.log(apiUrl);
+
+  if (movieName.length <= 0) {
+    containerNotFound.classList.add("display");
+    containerResult.classList.add("display");
+  } else {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.response === "true") {
+          console.log(data);
+        } else {
+          console.log("no data");
+        }
+      });
+  }
+};
+
+btnSearch.addEventListener("click", () => {
+  getMovie();
+});
