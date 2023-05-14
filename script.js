6184b5b0f64ac537f7f6be8aff1f68f3eac011c3
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-05-14 12:45:24 +0200

    added keypress enter to search the value, and initiated Enter key sensetive

--- script.js
+++ script.js
@@ -5,32 +5,31 @@ let btnSearch = document.getElementById("btn-search");
 const btnRefresh = document.querySelector(".btn-refresh");
 let containerResult = document.querySelector(".result");
 const containerNotFound = document.querySelector(".not-found");
-
 const apiKey = "4fccaa42";
 
 // -----------------------------------------//
 
-// reloading btn in not-found section
+// reloading btn in not-found section----//
 
 btnRefresh.addEventListener("click", () => {
   window.location.reload();
 });
 
-// function for fetching from API---------
+// function for fetching from API---------//
 
 const getMovie = () => {
   const movieName = movieNameRef.value;
   const apiUrl = `https://www.omdbapi.com/?i=${movieName}&apikey=${apiKey}`;
   console.log(apiUrl);
 
-  if (movieName.length <= 0) {
+  if (movieName === "") {
     containerNotFound.classList.add("display");
     containerResult.classList.add("display");
   } else {
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => {
-        if (data.response === "true") {
+        if (data.Response === "True") {
           console.log(data);
         } else {
           console.log("no data");
@@ -39,6 +38,16 @@ const getMovie = () => {
   }
 };
 
+// Button search handler-----------------------//
+
 btnSearch.addEventListener("click", () => {
   getMovie();
+});
+
+const keyRefresh = window.addEventListener("keypress", (e) => {
+  if (e === "Enter") {
+    getMovie();
+  } else {
+    return;
+  }
 });
