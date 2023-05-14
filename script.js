d89fb87f7f34768b8dd0b2b6689a1ab4fb65e88f
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-05-14 18:59:35 +0200

    added budget into result section and fixing some style in result after fecthing API

--- script.js
+++ script.js
@@ -35,18 +35,20 @@ const getMovie = () => {
           console.log(data);
           containerResult.classList.add("display");
           containerResult.innerHTML = `<div class="info">
-          <div class="info__img">
+          <div class="info__img" alt="movie poster">
            <img src=${data.Poster} class="info__poster">
             </div>
            <div class="info__detail">
              <h3 class="info__title">${data.Title} <span>(${data.Year})</span> </h3>
+             <p class="info__type"><span class="info__key">Type:</span> ${data.Type}</p>
              <p class="info__language"><span class="info__key">Language:</span> ${data.Language}</p>
              <p class="info__release"><span class="info__key">Release:</span> ${data.Released}</p>
               <p class="info__duration"><span class="info__key">Duration:</span> ${data.Runtime}</p>
               <p class="info__rate"><span class="info__key">imdb rate:</span> <i class="bi   bi-star-fill"></i>${data.imdbRating}</p>
-              <p class="info__plot"> ${data.Plot}</p>
+              <p class="info__plot"><span class="info__key">Story:</span> ${data.Plot}</p>
               <p class="info__awards"><span class="info__key">Awarded:</span> ${data.Awards}</p>
               <p class="info__actors"><span class="info__key">Actors:</span> ${data.Actors}</p>
+              <p class="info__budget"><span class="info__key">Budget:</span> ${data.BoxOffice}</p>
               
            </div>
           </div>
